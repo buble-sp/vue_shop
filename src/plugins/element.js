b2e6dd26ef19981c98e38f8264e66a3c4e6aa4d7
buble-sp <junbin1203@hotmail.com>
2021-07-29 15:11:43 +0800

    完成登录功能

---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(Input)
+Vue.use(FormItem)
+Vue.prototype.$message = Message
