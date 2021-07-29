cff045701fc852aef53b94ee79408706644f49b7
buble-sp <junbin1203@hotmail.com>
2021-07-29 17:28:08 +0800

    侧边栏写到一半

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Aside, Header, Main, Menu, Submenu, MenuItem } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(Input)
 Vue.use(FormItem)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Aside)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Submenu)
 Vue.prototype.$message = Message
